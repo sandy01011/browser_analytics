2649001137722d3787ff60f9c78648c264ac2b11
sandy01011 <sk2011mishra@gmail.com>
2021-12-11 14:29:48 +0100

    added logger module

---
+++ bd_parser.py
@@ -0,0 +1,13 @@
+# This file will have browsing data parser class
+
+
+
+class BdParser(object):
+    def __init__(self):
+        pass
+
+    def PreParser(self):
+        pass
+
+    def RawParser(self):
+        pass
