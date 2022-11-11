f9100e11e1532cb3805e666b5907947f5e00a1b5
sk2011mishra@gmail.com <sk2011mishra@gmail.com>
2022-11-12 00:50:31 +0530

    bran engine

--- browser_profiles.sh
+++ browser_profiles.sh
@@ -1,11 +1,11 @@
-pro_def_src="/home/maruti/.config/google-chrome/Default/History"
+pro_def_src="/home/maruti/.config/google-chrome/"
 pro_1_src="/home/maruti/.config/google-chrome/Profile 1/History"
 pro_2_src="/home/maruti/.config/google-chrome/Profile 2/History"
 pro_dst_path="/RBKP0/kala_hdd_aiml_lab-19/skm_gitops/github/browser_analytics/raw_data/raw_profiles"
 date=$(date +"%m%d%y")
-cp $pro_def_src $pro_dst_path/'PDHistory_'$date
-cp $pro_1_src $pro_dst_path/'P1History_'$date
-cp $pro_2_src $pro_dst_path/'P2History_'$date
+cp $pro_def_src/Default/History $pro_dst_path/'PDHistory_'$date
+cp $pro_def_src/Profile\ 1/History $pro_dst_path/'P1History_'$date
+cp $pro_def_src/Profile\ 2/History $pro_dst_path/'P2History_'$date
 # path_src=./folder1
 # path_dst=./folder2
 # date=$(date +"%m%d%y")
