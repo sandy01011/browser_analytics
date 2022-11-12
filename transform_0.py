b4bbcca74b39c5cfb2200214bb40b55a026b388a
sk2011mishra@gmail.com <sk2011mishra@gmail.com>
2022-11-12 23:22:15 +0530

    transformaton

--- transform_0.py
+++ transform_0.py
@@ -1,3 +1,8 @@
 # This file will transform data
 
+# data cleaning, and composition
 # remove dublicates
+
+# EDA reports TimeSeries
+
+# enrichment with doc_type (HTML,pdf,csv,vedio etc.) classfication and EDA enrichments
